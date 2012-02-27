104b74c8295258401284918579d40cc9e9e1a082
mattray <matthewhray@gmail.com>
2012-02-27 16:24:24 -0600

    add support for listing virtual cpus

--- lib/fog/openstack/requests/compute/get_flavor_details.rb
+++ lib/fog/openstack/requests/compute/get_flavor_details.rb
@@ -8,7 +8,7 @@ module Fog
             :expects  => [200, 203],
             :method   => 'GET',
             :path     => "flavors/#{flavor_ref}.json"
-          )
+            )
         end
 
       end
@@ -18,13 +18,13 @@ module Fog
         def get_flavor_details(flavor_ref)
           response = Excon::Response.new
           flavor = {
-            '1' => { 'id' => '1', 'name' => '256 server', 'ram' => 256, 'disk' => 10, 'links' => [] },
-            '2' => { 'id' => '2', 'name' => '512 server', 'ram' => 512, 'disk' => 20, 'links' => [] },
-            '3' => { 'id' => '3', 'name' => '1GB server', 'ram' => 1024, 'disk' => 40, 'links' => [] },
-            '4' => { 'id' => '4', 'name' => '2GB server', 'ram' => 2048, 'disk' => 80, 'links' => [] },
-            '5' => { 'id' => '5', 'name' => '4GB server', 'ram' => 4096, 'disk' => 160, 'links' => [] },
-            '6' => { 'id' => '6', 'name' => '8GB server', 'ram' => 8192, 'disk' => 320, 'links' => [] },
-            '7' => { 'id' => '7', 'name' => '15.5GB server', 'ram' => 15872, 'disk' => 620, 'links' => [] }
+            '1' => { 'id' => '1', 'name' => '256 server', 'ram' => 256, 'disk' => 10, 'vcpus' => 1, 'links' => [] },
+            '2' => { 'id' => '2', 'name' => '512 server', 'ram' => 512, 'disk' => 20, 'vcpus' => 1, 'links' => [] },
+            '3' => { 'id' => '3', 'name' => '1GB server', 'ram' => 1024, 'disk' => 40, 'vcpus' => 1, 'links' => [] },
+            '4' => { 'id' => '4', 'name' => '2GB server', 'ram' => 2048, 'disk' => 80, 'vcpus' => 2, 'links' => [] },
+            '5' => { 'id' => '5', 'name' => '4GB server', 'ram' => 4096, 'disk' => 160, 'vcpus' => 2, 'links' => [] },
+            '6' => { 'id' => '6', 'name' => '8GB server', 'ram' => 8192, 'disk' => 320, 'vcpus' => 4, 'links' => [] },
+            '7' => { 'id' => '7', 'name' => '15.5GB server', 'ram' => 15872, 'disk' => 620, 'vcpus' => 8, 'links' => [] }
           }[flavor_ref]
           if flavor
             response.status = 200
--- lib/fog/openstack/requests/compute/list_flavors_detail.rb
+++ lib/fog/openstack/requests/compute/list_flavors_detail.rb
@@ -8,7 +8,7 @@ module Fog
             :expects  => [200, 203],
             :method   => 'GET',
             :path     => 'flavors/detail.json'
-          )
+            )
         end
 
       end
@@ -20,13 +20,13 @@ module Fog
           response.status = 200
           response.body = {
             'flavors' => [
-              { 'name' => '256 server', 'id' => '1', 'ram' => 256, 'disk' => 10, 'links' => [] },
-              { 'name' => '512 server', 'id' => '2', 'ram' => 512, 'disk' => 20, 'links' => [] },
-              { 'name' => '1GB server', 'id' => '3', 'ram' => 1024, 'disk' => 40, 'links' => [] },
-              { 'name' => '2GB server', 'id' => '4', 'ram' => 2048, 'disk' => 80, 'links' => [] },
-              { 'name' => '4GB server', 'id' => '5', 'ram' => 4096, 'disk' => 160, 'links' => [] },
-              { 'name' => '8GB server', 'id' => '6', 'ram' => 8192, 'disk' => 320, 'links' => [] },
-              { 'name' => '15.5GB server', 'id' => '7', 'ram' => 15872, 'disk' => 620, 'links' => [] }
+              { 'name' => '256 server', 'id' => '1', 'ram' => 256, 'disk' => 10, 'vcpus' => 1, 'links' => [] },
+              { 'name' => '512 server', 'id' => '2', 'ram' => 512, 'disk' => 20, 'vcpus' => 1, 'links' => [] },
+              { 'name' => '1GB server', 'id' => '3', 'ram' => 1024, 'disk' => 40, 'vcpus' => 1, 'links' => [] },
+              { 'name' => '2GB server', 'id' => '4', 'ram' => 2048, 'disk' => 80, 'vcpus' => 2, 'links' => [] },
+              { 'name' => '4GB server', 'id' => '5', 'ram' => 4096, 'disk' => 160, 'vcpus' => 2, 'links' => [] },
+              { 'name' => '8GB server', 'id' => '6', 'ram' => 8192, 'disk' => 320, 'vcpus' => 4, 'links' => [] },
+              { 'name' => '15.5GB server', 'id' => '7', 'ram' => 15872, 'disk' => 620, 'vcpus' => 8, 'links' => [] }
             ]
           }
           response
